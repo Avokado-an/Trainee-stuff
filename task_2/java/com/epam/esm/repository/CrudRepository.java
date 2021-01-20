c4b41ef21bbf74264a0a4102ebb0d927634577d8
Avokado-an <56486448+Avokado-an@users.noreply.github.com>
2021-01-20 14:50:45 +0300

    ooops again

--- task_2/java/com/epam/esm/repository/CrudRepository.java
+++
@@ -1,7 +0,0 @@
-package com.epam.esm.repository;
-
-import java.util.Optional;
-
-public interface CrudRepository<T> extends CrdRepository<T> {
-    Optional<T> update(T newInstance);
-}
--- task_2/src/test/resources/query/fill_certificate_tag_table.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO gift_certificate_tag(gift_certificate_id, tag_id)
-VALUES (1, 1);
-INSERT INTO gift_certificate_tag(gift_certificate_id, tag_id)
-VALUES (2, 3);
-INSERT INTO gift_certificate_tag(gift_certificate_id, tag_id)
-VALUES (1, 2);
-INSERT INTO gift_certificate_tag(gift_certificate_id, tag_id)
-VALUES (2, 2);
