1f4fef44a7e9a5d57682382b9b89252f24cadb99
Avokado-an <56486448+Avokado-an@users.noreply.github.com>
2021-01-20 14:49:49 +0300

    ooops

--- Task2/controller/src/main/java/com/epam/esm/error/ErrorHandler.java
+++
@@ -1,9 +0,0 @@
-package com.epam.esm.error;
-
-import lombok.AllArgsConstructor;
-
-@AllArgsConstructor
-public class ErrorHandler {
-    private final String errorMessage;
-    private final int errorCode;
-}
